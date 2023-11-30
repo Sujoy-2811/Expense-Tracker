1b28f88074526b786e3ed9f64b6777393c7fdc59
Sujoy Manna <sujoymanna007@gmail.com>
2023-11-30 22:55:02 +0530

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 // import viteLogo from "/vite.svg";
+import { useEffect } from "react";
 import "./App.css";
+import authService from "./appwrite/auth";
 
 export default function App() {
+  // useEffect(() => {
+  //   console.log("hi");
+  //   authService.createAccount({
+  //     email: "test@test.com",
+  //     password: "12345678",
+  //     name: "test",
+  //   });
+  // }, []);
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
